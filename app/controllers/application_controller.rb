4c45c4c9d7ef93bf5838d930e8e522dc271f2bae
Diego Oliveira <diegoitarare@gmail.com>
2019-11-05 11:59:56 -0300

    Implements authetication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate!
+
+  private
+
+  def authenticate!
+    authenticate_with_http_token do |token, _|
+      @current_account ||= fetch_account(token, params[:account_id])
+    end || head(:unauthorized)
+  end
+
+  def fetch_account(token, id)
+    Account.joins(:customer)
+           .where('customers.access_token = ? AND accounts.id = ?', token, id)
+           .first
+  end
 end
